96c2b8d11709b91e35cceb044794ebf386f8e14e
majaliju <majalijunyc@gmail.com>
2022-08-07 19:23:53 -0400

    updated Home and other files

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Home from './Home'
 
 
 function App() {
   return (
    <ChakraProvider>
-
+    <Home />
    </ChakraProvider>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Box, Text, Center} from "@chakra-ui/react"
+
+function Home() {
+  return (
+    <Box>
+      <Center>
+      <Text>
+        THIS IS THE HOME PAGE!
+      </Text>
+      </Center>
+    </Box>
+  )
+}
+
+export default Home;
