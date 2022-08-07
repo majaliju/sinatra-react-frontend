90978cb95df581e7b8d20bdac544602608bf42e3
majaliju <majalijunyc@gmail.com>
2022-08-07 19:29:43 -0400

    updated concept on the App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Home from './Home'
 
 
+// the idea for this App is where people leave reviews
+// for the greatest songs of all time contest
+// where people can enter their favorites
+
+
 function App() {
   return (
    <ChakraProvider>
