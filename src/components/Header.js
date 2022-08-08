1c198bb14a88ea77282627f1993f6253b448a423
majaliju <majalijunyc@gmail.com>
2022-08-07 22:50:24 -0400

    updated the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,59 @@
-import {Box, Text, Center} from "@chakra-ui/react";
+import { ReactNode } from 'react';
+import {
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center, Text, Box
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+
 
 function Header() {
+
+  const NavLink = ({ children }: { children: ReactNode }) => (
+    <Link
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+      href={'#'}>
+      {children}
+    </Link>
+  );
+  
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  
   return (
     <div>
-      <Box>
-      <Center>
-      <Text>
-        THIS IS THE HOME PAGE!
-      </Text>
-      </Center>
-    </Box>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Box>SONG RANKINGS</Box>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
     </div>
   )
 }
