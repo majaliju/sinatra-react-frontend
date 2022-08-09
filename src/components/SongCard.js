09b1dd36caf0fe3469aed45771eafb961a78a81d
majaliju <majalijunyc@gmail.com>
2022-08-08 21:55:38 -0400

    updated SongCard, Header and imported songs & reviews states

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,62 +1,60 @@
-import { Box, Image, Badge } from '@chakra-ui/react'
+import { Box, Image, Badge } from "@chakra-ui/react";
 
 /* NOT THE IDEAL BUT A LITTLE SKETCH OF HOW THE SONGS AND REVIEWS SHOULD THEMSELVES POP UP FOR DISPLAY */
 
-
 /* THE PROPERTY PROP SHOULD BE THE SONG PROP WITH PROPER VALUES INSTEAD */
-function SongCard({songs, setSongs, reviews, setReviews}) {
-
-  // create map functions for the reviews -- one card per review
-    
+function SongCard({ songs, setSongs, reviews, setReviews }) {
+  // create map functions for the individual songs and show reviews for each
 
   return (
-    <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' bgColor = 'beige'>
-
-      <Box p='6'>
-        <Box display='flex' alignItems='baseline'>
-          <Badge borderRadius='full' px='6' colorScheme='blue'>
-            LIKE
-          </Badge>
-          <Badge borderRadius='full' px='5' colorScheme='red'>
-            DISLIKE
-          </Badge>
-        </Box>
-
+    <Box
+      maxW="sm"
+      borderWidth="2px"
+      borderRadius="lg"
+      overflow="hidden"
+      
+    >
+      <Box p="5">
         <Box
-          mt='1'
-          fontWeight='extrabold'
-          as='h4'
-          lineHeight='tight'
+          mt="1"
+          fontWeight="thin"
+          as="h4"
+          lineHeight="tight"
           noOfLines={2}
-          bgColor = 'gold'
-        >
-        </Box>
-        <Box
-          mt='1'
-          fontWeight='extrabold'
-          as='h4'
-          lineHeight='tight'
-          noOfLines={2}
-          bgColor = 'gold'
         >
           SONG NAME
         </Box>
         <Box
-          mt='1'
-          fontWeight='extrabold'
-          as='h4'
-          lineHeight='tight'
+          mt="1"
+          fontWeight="thin"
+          as="h4"
+          lineHeight="tight"
           noOfLines={2}
-          bgColor = 'gold'
+        >
+          ARTIST NAME
+        </Box>
+        <Box
+          mt="1"
+          fontWeight="thin"
+          as="h4"
+          lineHeight="tight"
+          noOfLines={2}
         >
           GENRE
         </Box>
-          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-             {reviews}
+        <Box as="span" ml="2" color="gray.600" fontSize="sm">
+          <Box display="flex" alignItems="baseline">
+            <Badge borderRadius="full" fontWeight="normal" px="5" colorScheme="blue">
+              LIKE
+            </Badge>
+            <Badge borderRadius="full" fontWeight="normal" px="5" colorScheme="red">
+              DISLIKE
+            </Badge>
           </Box>
+        </Box>
       </Box>
     </Box>
-  )
+  );
 }
 
 export default SongCard;
