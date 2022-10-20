9d7557764c844ba94850dcc939f14d17bdd32faa
majaliju <majalijunyc@gmail.com>
2022-10-20 17:36:00 -0400

    added search state; wrote in the .filter.map method

--- src/components/SongDisplay.js
+++ src/components/SongDisplay.js
@@ -121,14 +121,10 @@ function SongsDisplay() {
       .then((info) => setReviews([...reviews, info]));
   }
 
-  //^ TODO
-  //^ need to optimize both lower function into a single functions for D.R.Y. principles
-  //^ a single updateLikes with a state variable that sets itself based on which button is clicked
-
-  // each references which review
-  // type will have either {likes} or {dislikes} as the value
-  // question is: should type be in state?
-  // will cause a re-render everytime if so; on other hand, will be very up to date
+  // wipes search term clean; to reset the slate on each reload
+  useEffect(() => {
+    setSearch('');
+  }, []);
 
   // updates the likes per click on LIKE button
   function updateReviewLikes(each) {
@@ -199,153 +195,164 @@ function SongsDisplay() {
       <Box id='songDisplayBody'>
         <Box>
           <AddNewSong submitNewSong={submitNewSong} />
-          {songs.map((song) => (
-            <Box
-              key={song.id}
-              maxW='xl'
-              borderWidth='2px'
-              borderRadius='lg'
-              overflow='hidden'
-              fontFamily='Helvetica'>
-              <Box p='2'>
-                <Box
-                  mt='1'
-                  fontWeight='thin'
-                  fontSize='3xl'
-                  as='h2'
-                  lineHeight='tight'
-                  noOfLines={2}>
-                  {song.name.toUpperCase()}
-                </Box>
-                <Box
-                  mt='1'
-                  fontWeight='thin'
-                  fontSize='2xl'
-                  as='h1'
-                  lineHeight='tight'
-                  noOfLines={2}>
-                  {song.artist.name.toUpperCase()}
-                </Box>
-                <Box
-                  mt='1'
-                  fontWeight='thin'
-                  fontSize='xl'
-                  as='h1'
-                  lineHeight='tight'
-                  noOfLines={2}>
-                  {song.genre.name.toUpperCase()}
-                </Box>
-                <Box
-                  mt='1'
-                  fontWeight='thin'
-                  fontSize='lg'
-                  as='h1'
-                  lineHeight='tight'
-                  noOfLines={2}>
-                  {song.year}
-                </Box>
+          {songs
+            .filter((song) => {
+              if (search === '') {
+                return song;
+              } else if (
+                song.name.toLowerCase().includes(search.toLowerCase()) ||
+                song.genre.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return song;
+              }
+            })
+            .map((song) => (
+              <Box
+                key={song.id}
+                maxW='xl'
+                borderWidth='2px'
+                borderRadius='lg'
+                overflow='hidden'
+                fontFamily='Helvetica'>
+                <Box p='2'>
+                  <Box
+                    mt='1'
+                    fontWeight='thin'
+                    fontSize='3xl'
+                    as='h2'
+                    lineHeight='tight'
+                    noOfLines={2}>
+                    {song.name.toUpperCase()}
+                  </Box>
+                  <Box
+                    mt='1'
+                    fontWeight='thin'
+                    fontSize='2xl'
+                    as='h1'
+                    lineHeight='tight'
+                    noOfLines={2}>
+                    {song.artist.name.toUpperCase()}
+                  </Box>
+                  <Box
+                    mt='1'
+                    fontWeight='thin'
+                    fontSize='xl'
+                    as='h1'
+                    lineHeight='tight'
+                    noOfLines={2}>
+                    {song.genre.name.toUpperCase()}
+                  </Box>
+                  <Box
+                    mt='1'
+                    fontWeight='thin'
+                    fontSize='lg'
+                    as='h1'
+                    lineHeight='tight'
+                    noOfLines={2}>
+                    {song.year}
+                  </Box>
 
-                <Flex
-                  w='full'
-                  bg='#edf3f8'
-                  _dark={{
-                    bg: '#3e3e3e',
-                  }}
-                  p={1}
-                  alignItems='center'
-                  justifyContent='center'>
-                  <Stack
-                    direction={{
-                      base: 'column',
-                    }}
+                  <Flex
                     w='full'
-                    shadow='2xl'>
-                    {reviews
-                      .filter(
-                        (review) =>
-                          parseInt(review.song_id) === parseInt(song.id)
-                      )
-                      .map((each) => (
-                        <Flex
-                          direction={{
-                            base: 'row',
-                            md: 'column',
-                          }}
-                          bg='beige'
-                          key={each.id}>
-                          <SimpleGrid
-                            spacingY={3}
-                            columns={{
-                              base: 1,
-                              md: 1,
-                            }}
-                            w={{
-                              base: 120,
-                              md: 'full',
+                    bg='#edf3f8'
+                    _dark={{
+                      bg: '#3e3e3e',
+                    }}
+                    p={1}
+                    alignItems='center'
+                    justifyContent='center'>
+                    <Stack
+                      direction={{
+                        base: 'column',
+                      }}
+                      w='full'
+                      shadow='2xl'>
+                      {reviews
+                        .filter(
+                          (review) =>
+                            parseInt(review.song_id) === parseInt(song.id)
+                        )
+                        .map((each) => (
+                          <Flex
+                            direction={{
+                              base: 'row',
+                              md: 'column',
                             }}
                             bg='beige'
-                            color='black'
-                            py={{
-                              base: 1,
-                              md: 4,
-                            }}
-                            px={{
-                              base: 2,
-                              md: 2,
-                            }}
-                            fontSize='md'
-                            fontWeight='normal'
-                            fontFamily='Helvetica'>
-                            <span>{each.comment}</span>
-                          </SimpleGrid>
-                          <Flex
-                            justify={{
-                              md: 'end',
-                            }}>
-                            <Button
-                              variant='solid'
-                              colorScheme='blue'
-                              size='sm'
-                              onClick={() => {
-                                updateReviewLikes(each);
+                            key={each.id}>
+                            <SimpleGrid
+                              spacingY={3}
+                              columns={{
+                                base: 1,
+                                md: 1,
+                              }}
+                              w={{
+                                base: 120,
+                                md: 'full',
+                              }}
+                              bg='beige'
+                              color='black'
+                              py={{
+                                base: 1,
+                                md: 4,
+                              }}
+                              px={{
+                                base: 2,
+                                md: 2,
+                              }}
+                              fontSize='md'
+                              fontWeight='normal'
+                              fontFamily='Helvetica'>
+                              <span>{each.comment}</span>
+                            </SimpleGrid>
+                            <Flex
+                              justify={{
+                                md: 'end',
                               }}>
-                              {each.likes} LIKES
-                            </Button>
-                            <Button
-                              variant='solid'
-                              colorScheme='red'
-                              size='sm'
-                              onClick={() => {
-                                updateReviewDislikes(each);
-                              }}>
-                              {each.dislikes} DISLIKES
-                            </Button>
+                              <Button
+                                variant='solid'
+                                colorScheme='blue'
+                                size='sm'
+                                onClick={() => {
+                                  updateReviewLikes(each);
+                                }}>
+                                {each.likes} LIKES
+                              </Button>
+                              <Button
+                                variant='solid'
+                                colorScheme='red'
+                                size='sm'
+                                onClick={() => {
+                                  updateReviewDislikes(each);
+                                }}>
+                                {each.dislikes} DISLIKES
+                              </Button>
+                            </Flex>
                           </Flex>
-                        </Flex>
-                      ))}
-                  </Stack>
-                </Flex>
-                <AddNewReview
-                  submitNewReview={submitNewReview}
-                  songID={song.id}
-                  reviews={reviews}
-                />
-                <UpdateSong updateThisSong={updateThisSong} song={song} />
+                        ))}
+                    </Stack>
+                  </Flex>
+                  <AddNewReview
+                    submitNewReview={submitNewReview}
+                    songID={song.id}
+                    reviews={reviews}
+                  />
+                  <UpdateSong updateThisSong={updateThisSong} song={song} />
 
-                {/* DELETE THIS SONG button */}
-                <Button
-                  variant='solid'
-                  colorScheme='orange'
-                  size='sm'
-                  w='100%'
-                  onClick={() => {
-                    deleteSong(song);
-                  }}>
-                  DELETE THIS SONG (WHY DID I CREATE THIS BUTTON??)
-                </Button>
+                  {/* DELETE THIS SONG button */}
+                  <Button
+                    variant='solid'
+                    colorScheme='orange'
+                    size='sm'
+                    w='100%'
+                    onClick={() => {
+                      deleteSong(song);
+                    }}>
+                    DELETE THIS SONG (WHY DID I CREATE THIS BUTTON??)
+                  </Button>
+                </Box>
               </Box>
-            </Box>
-          ))}
+            ))}
         </Box>
       </Box>
       <Box as='i' id='songStats' fontFamily='Helvetica' fontWeight='thin'>
